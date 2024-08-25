af7613cf123aa3e900c7e1c6739f51d40ffe84a9
Nouman Mahmood <noumanmhd@outlook.com>
2024-08-25 09:10:00 +0300

    feat:  Fork from https://github.com/blockscout/frontend/releases/tag/v1.33.2

---
+++ ui/home/Stats.tsx
@@ -0,0 +1,165 @@
+import { Grid } from '@chakra-ui/react';
+import BigNumber from 'bignumber.js';
+import React from 'react';
+
+import config from 'configs/app';
+import useApiQuery from 'lib/api/useApiQuery';
+import { WEI } from 'lib/consts';
+import { HOMEPAGE_STATS } from 'stubs/stats';
+import GasInfoTooltip from 'ui/shared/gas/GasInfoTooltip';
+import GasPrice from 'ui/shared/gas/GasPrice';
+import IconSvg from 'ui/shared/IconSvg';
+import type { Props as StatsWidgetProps } from 'ui/shared/stats/StatsWidget';
+import StatsWidget from 'ui/shared/stats/StatsWidget';
+
+const hasAvgBlockTime = config.UI.homepage.showAvgBlockTime;
+const rollupFeature = config.features.rollup;
+
+const Stats = () => {
+  const [ hasGasTracker, setHasGasTracker ] = React.useState(config.features.gasTracker.isEnabled);
+  const { data, isPlaceholderData, isError, dataUpdatedAt } = useApiQuery('stats', {
+    queryOptions: {
+      refetchOnMount: false,
+      placeholderData: HOMEPAGE_STATS,
+    },
+  });
+
+  React.useEffect(() => {
+    if (!isPlaceholderData && !data?.gas_prices?.average) {
+      setHasGasTracker(false);
+    }
+  // should run only after initial fetch
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ isPlaceholderData ]);
+
+  const zkEvmLatestBatchQuery = useApiQuery('homepage_zkevm_latest_batch', {
+    queryOptions: {
+      placeholderData: 12345,
+      enabled: rollupFeature.isEnabled && rollupFeature.type === 'zkEvm',
+    },
+  });
+
+  const zkSyncLatestBatchQuery = useApiQuery('homepage_zksync_latest_batch', {
+    queryOptions: {
+      placeholderData: 12345,
+      enabled: rollupFeature.isEnabled && rollupFeature.type === 'zkSync',
+    },
+  });
+
+  if (isError || zkEvmLatestBatchQuery.isError || zkSyncLatestBatchQuery.isError) {
+    return null;
+  }
+
+  const isLoading = isPlaceholderData ||
+    (rollupFeature.isEnabled && rollupFeature.type === 'zkEvm' && zkEvmLatestBatchQuery.isPlaceholderData) ||
+    (rollupFeature.isEnabled && rollupFeature.type === 'zkSync' && zkSyncLatestBatchQuery.isPlaceholderData);
+
+  const content = (() => {
+    if (!data) {
+      return null;
+    }
+    const gasInfoTooltip = hasGasTracker && data.gas_prices && data.gas_prices.average ? (
+      <GasInfoTooltip data={ data } dataUpdatedAt={ dataUpdatedAt }>
+        <IconSvg
+          isLoading={ isLoading }
+          name="info"
+          boxSize={ 5 }
+          flexShrink={ 0 }
+          cursor="pointer"
+          color="icon_info"
+          _hover={{ color: 'link_hovered' }}
+        />
+      </GasInfoTooltip>
+    ) : null;
+
+    const items: Array<StatsWidgetProps> = [
+      rollupFeature.isEnabled && rollupFeature.type === 'zkEvm' && {
+        icon: 'txn_batches_slim' as const,
+        label: 'Latest batch',
+        value: (zkEvmLatestBatchQuery.data || 0).toLocaleString(),
+        href: { pathname: '/batches' as const },
+        isLoading,
+      },
+      rollupFeature.isEnabled && rollupFeature.type === 'zkSync' && {
+        icon: 'txn_batches_slim' as const,
+        label: 'Latest batch',
+        value: (zkSyncLatestBatchQuery.data || 0).toLocaleString(),
+        href: { pathname: '/batches' as const },
+        isLoading,
+      },
+      !(rollupFeature.isEnabled && (rollupFeature.type === 'zkEvm' || rollupFeature.type === 'zkSync')) && {
+        icon: 'block_slim' as const,
+        label: 'Total blocks',
+        value: Number(data.total_blocks).toLocaleString(),
+        href: { pathname: '/blocks' as const },
+        isLoading,
+      },
+      hasAvgBlockTime && {
+        icon: 'clock-light' as const,
+        label: 'Average block time',
+        value: `${ (data.average_block_time / 1000).toFixed(1) }s`,
+        isLoading,
+      },
+      {
+        icon: 'transactions_slim' as const,
+        label: 'Total transactions',
+        value: Number(data.total_transactions).toLocaleString(),
+        href: { pathname: '/txs' as const },
+        isLoading,
+      },
+      rollupFeature.isEnabled && data.last_output_root_size && {
+        icon: 'txn_batches_slim' as const,
+        label: 'Latest L1 state batch',
+        value: data.last_output_root_size,
+        href: { pathname: '/batches' as const },
+        isLoading,
+      },
+      {
+        icon: 'wallet' as const,
+        label: 'Wallet addresses',
+        value: Number(data.total_addresses).toLocaleString(),
+        isLoading,
+      },
+      hasGasTracker && data.gas_prices && {
+        icon: 'gas' as const,
+        label: 'Gas tracker',
+        value: data.gas_prices.average ? <GasPrice data={ data.gas_prices.average }/> : 'N/A',
+        hint: gasInfoTooltip,
+        isLoading,
+      },
+      data.rootstock_locked_btc && {
+        icon: 'coins/bitcoin' as const,
+        label: 'BTC Locked in 2WP',
+        value: `${ BigNumber(data.rootstock_locked_btc).div(WEI).dp(0).toFormat() } RBTC`,
+        isLoading,
+      },
+    ].filter(Boolean);
+
+    return (
+      <>
+        { items.map((item, index) => (
+          <StatsWidget
+            key={ item.icon }
+            { ...item }
+            isLoading={ isLoading }
+            _last={ items.length % 2 === 1 && index === items.length - 1 ? { gridColumn: 'span 2' } : undefined }/>
+        ),
+        ) }
+      </>
+    );
+  })();
+
+  return (
+    <Grid
+      gridTemplateColumns="1fr 1fr"
+      gridGap={{ base: 1, lg: 2 }}
+      flexBasis="50%"
+      flexGrow={ 1 }
+    >
+      { content }
+    </Grid>
+
+  );
+};
+
+export default Stats;
