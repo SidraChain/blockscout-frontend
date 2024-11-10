ee5598a14b40e3cbe1c2f09ba4de1a8ed8976f33
Nouman Mahmood <noumanmhd@outlook.com>
2024-11-10 19:54:32 +0300

    Improve balance reprensentaion

--- lib/getCurrencyValue.ts
+++ lib/getCurrencyValue.ts
@@ -11,6 +11,12 @@ interface Params {
 }
 
 export default function getCurrencyValue({ value, accuracy, accuracyUsd, decimals, exchangeRate }: Params) {
+  // Max 256-bit value (2^256 - 1)
+  const MAX_255_BIT = new BigNumber(2).pow(255).minus(1);
+  // Check if value is close to max 255-bit
+  if (new BigNumber(value).gte(MAX_255_BIT)) {
+    return { valueStr: '∞', usd: undefined, usdBn: ZERO };
+  }
   const valueCurr = BigNumber(value).div(BigNumber(10 ** Number(decimals || '18')));
   const valueResult = accuracy ? valueCurr.dp(accuracy).toFormat() : valueCurr.toFormat();
 
